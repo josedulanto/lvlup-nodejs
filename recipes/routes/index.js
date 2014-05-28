24c55f0aaac3fdefb75253e515ce3c611255e373
José Dulanto <jose.dulanto@live.com>
2014-05-28 12:19:27 -0500

    moves files to a /simple folder and adds exercises from las videos of Lynda.com Node First Look

---
+++ recipes/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// router.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+module.exports = router;
